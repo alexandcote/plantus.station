69c48a98fd98e0e9a7a27dfb606d4b561c1cfe25
Alexandre Côté <a.cote@2kloc.com>
2017-01-22 01:19:27 -0500

    Try to connect with the PC

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "mbed.h"
 
+Serial pc(USBTX, USBRX);
 DigitalOut led1(LED1);
 
 // main() runs in its own thread in the OS
 // (note the calls to wait below for delays)
 int main() {
     while (true) {
+        pc.getc();
         led1 = !led1;
         wait(0.1);
+        led1 = !led1;
     }
 }
 
